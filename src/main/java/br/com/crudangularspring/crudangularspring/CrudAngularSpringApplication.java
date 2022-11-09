75c0d218180bbb3b6c331185a50ba2591902d1c8
Rennan Morais <rennan.morais@outlook.com>
2022-11-08 22:48:54 -0300

    Signed-off-by: Rennan Morais <rennan.morais@outlook.com>

---
+++ src/main/java/br/com/crudangularspring/crudangularspring/CrudAngularSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.crudangularspring.crudangularspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAngularSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crudangularspring/crudangularspring/CrudAngularSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crudangularspring.crudangularspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAngularSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
