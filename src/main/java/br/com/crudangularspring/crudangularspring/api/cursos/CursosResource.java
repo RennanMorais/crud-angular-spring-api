92156a0b1b43fafb73cec8bab6865f3a80332925
Rennan Morais <rennan.morais@outlook.com>
2022-11-15 16:43:12 -0300

    incluido primeiro endpoint de listar cursos

--- src/main/java/br/com/crudangularspring/crudangularspring/api/cursos/CursosResource.java
+++ src/main/java/br/com/crudangularspring/crudangularspring/api/cursos/CursosResource.java
@@ -1,5 +1,17 @@
 package br.com.crudangularspring.crudangularspring.api.cursos;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.crudangularspring.crudangularspring.api.cursos.dto.CursoResponseDTO;
+
+@RestController
+@RequestMapping("/crud-angular-api/cursos")
 public class CursosResource {
 
+	
+	public CursoResponseDTO listarCursos() {
+		return null;
+	}
+	
 }
